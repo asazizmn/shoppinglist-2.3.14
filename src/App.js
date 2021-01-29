cc18773fc7b83fb237284ade2c6a9b7de0c65224
S. Aziz <asazizmn@gmail.com>
2021-01-29 11:21:10 +0800

    move items state into ShoppingList component

--- src/App.js
+++ src/App.js
@@ -8,32 +8,6 @@ import ShoppingList from './ShoppingList';
 
 
 class App extends React.Component {
-  
-  state = {
-    items: []
-  };
-
-
-  addItem = item => {
-    this.setState(oldState => ({
-
-      // utilising the spread operator to ensure old items are preserved
-      // ... and not overwritten
-      items: [...oldState.items, item]
-    }));
-  };
-
-
-  deleteLastItem = event => {
-    this.setState(prevState => ({ items: this.state.items.slice(0, -1) }));
-  };
-
-
-  noItemsFound = () => {
-    return this.state.items.length === 0;
-  };
-
-
   render() {
     return (
       <div className="App">
@@ -43,15 +17,7 @@ class App extends React.Component {
           title={'ReactND - Coding Practice'}
         />
 
-        <ShoppingList
-          addItem={this.addItem}
-          // value={this.state.value}
-          // handleChange={this.handleChange}
-          // inputIsEmpty={this.inputIsEmpty}
-          deleteLastItem={this.deleteLastItem}
-          noItemsFound={this.noItemsFound}
-          items={this.state.items}
-        />
+        <ShoppingList />
       </div>
     );
   }
--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AddItem from './AddItem';
 import DeleteItem from './DeleteItem';
@@ -6,27 +6,54 @@ import DisplayItems from './DisplayItems';
 
 
 
-const ShoppingList = props => (
-    <React.Fragment>
-        <h2>Shopping List</h2>
+class ShoppingList extends Component {
 
-        <AddItem
-            addItem={props.addItem}
-            // value={props.value}
-            // handleChange={props.handleChange}
-            // inputIsEmpty={props.inputIsEmpty}
-        />
+    state = {
+        items: []
+    };
 
-        <DeleteItem
-            deleteLastItem={props.deleteLastItem}
-            noItemsFound={props.noItemsFound}
-        />
 
-        <DisplayItems
-            items={props.items}
-        />
-    </React.Fragment>
-);
+    addItem = item => {
+        this.setState(oldState => ({
+
+            // utilising the spread operator to ensure old items are preserved
+            // ... and not overwritten
+            items: [...oldState.items, item]
+        }));
+    };
+
+
+    deleteLastItem = event => {
+        this.setState(prevState => ({ items: this.state.items.slice(0, -1) }));
+    };
+
+
+    noItemsFound = () => {
+        return this.state.items.length === 0;
+    };
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <h2>Shopping List</h2>
+
+                <AddItem
+                    addItem={this.addItem}
+                />
+
+                <DeleteItem
+                    deleteLastItem={this.deleteLastItem}
+                    noItemsFound={this.noItemsFound}
+                />
+
+                <DisplayItems
+                    items={this.state.items}
+                />
+            </React.Fragment>
+        )
+    }
+}
 
 
 
